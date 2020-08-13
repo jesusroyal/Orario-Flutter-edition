c28b97e907182072d1d8ed411c6210270c36338b
Mike <m.shevelinski@ya.ru>
2020-08-13 16:25:30 +0300

    Cleaned setup_service

--- lib/services/setup_service.dart
+++ lib/services/setup_service.dart
@@ -5,13 +5,27 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SetupService {
   static var ref = FirebaseDatabase.instance.reference().child('uni');
 
+  static String _path;
+  static var lessonDict = Map<String, Lesson>();
+
+  static void init({
+    ready(bool needsLogin),
+  }) {
+    bool needsLogin;
+    if (_path == null) {
+      needsLogin = false;
+      ready(needsLogin);
+    } else {
+      needsLogin = true;
+      _getData(ready: ready(needsLogin));
+    }
+  }
+
   static List<String> univercityData = List<String>();
   static List<String> groupData = List<String>();
 
   static Map<String, String> _univercityDict = Map<String, String>();
   static Map<String, String> _groupDict = Map<String, String>();
-
-  static var lessonDict = Map<String, Lesson>();
 
   static void getUnivercityData({ready()}) {
     univercityData.clear();
@@ -23,7 +37,6 @@ class SetupService {
       });
       ready();
     });
-    print(_univercityDict);
   }
 
   static void getGroupDataFor({String univercity, ready()}) {
@@ -41,22 +54,26 @@ class SetupService {
     });
   }
 
-  static var didFinish = false;
-
-  static String _path;
-
-  static void init({
-    ready(bool needsLogin),
-  }) {
-    getPath();
-    bool needsLogin;
-    if (_path == null) {
-      needsLogin = false;
-      ready(needsLogin);
-    } else {
-      needsLogin = true;
-      _getData(ready: ready(needsLogin));
-    }
+  static void setGroupTo({String group, ready()}) {
+    _path += '/' + _groupDict[group];
+    ref = ref.child(_groupDict[group]).child("timetable");
+    ref.once().then((snapshot) {
+      for (int week = 0; week < 2; week++) {
+        for (int day = 0; day < snapshot.value[week.toString()].length; day++) {
+          for (int lesson = 0;
+              lesson < snapshot.value[week.toString()][day].length;
+              lesson++) {
+            if (snapshot.value[week.toString()][day][lesson] != null) {
+              var data = snapshot.value[week.toString()][day][lesson];
+              print(data);
+              lessonDict["$week/$day/$lesson"] = Lesson.fromDynamicMap(data);
+              print(lessonDict["$week/$day/$lesson"]);
+            }
+          }
+        }
+      }
+      ready();
+    });
   }
 
   static void _getData({ready()}) {
@@ -72,40 +89,6 @@ class SetupService {
               var data = snapshot.value[week.toString()][day][lesson];
               print(data);
               lessonDict["$week/$day/$lesson"] = Lesson.fromDynamicMap(data);
-            }
-          }
-        }
-      }
-      ready();
-    });
-  }
-
-  static getPath() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String stringValue = prefs.getString('path');
-    _path = stringValue;
-  }
-
-  static Future<void> setPath() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('path', _path);
-  }
-
-  static void setGroupTo({String group, ready()}) {
-    _path += '/' + _groupDict[group];
-    setPath();
-    ref = ref.child(_groupDict[group]).child("timetable");
-    ref.once().then((snapshot) {
-      for (int week = 0; week < 2; week++) {
-        for (int day = 0; day < snapshot.value[week.toString()].length; day++) {
-          for (int lesson = 0;
-              lesson < snapshot.value[week.toString()][day].length;
-              lesson++) {
-            if (snapshot.value[week.toString()][day][lesson] != null) {
-              var data = snapshot.value[week.toString()][day][lesson];
-              print(data);
-              lessonDict["$week/$day/$lesson"] = Lesson.fromDynamicMap(data);
-              print(lessonDict["$week/$day/$lesson"]);
             }
           }
         }
