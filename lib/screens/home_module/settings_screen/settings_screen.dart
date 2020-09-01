c58c134f83d261ccf8585b02453e09c7a4f75c4f
Mike <m.shevelinski@ya.ru>
2020-09-01 19:01:35 +0300

    Fixed settings design

--- lib/screens/home_module/settings_screen/settings_screen.dart
+++ lib/screens/home_module/settings_screen/settings_screen.dart
@@ -12,74 +12,78 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: OrarioColors.backGround,
-      child: ListView.builder(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Настройки'),
+      ),
+      body: ListView.builder(
           itemCount: 2,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(_settingsTitles[index]),
-              onTap: () {
-                switch (index) {
-                  case 0:
-                    OrarioService.resetSettings().then((value) {
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                          '/welcome', (route) => false);
-                    });
+            return Card(
+              child: ListTile(
+                title: Text(_settingsTitles[index]),
+                onTap: () {
+                  switch (index) {
+                    case 0:
+                      OrarioService.resetSettings().then((value) {
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                            '/welcome', (route) => false);
+                      });
 
-                    break;
-                  case 1:
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        String _token;
-                        return SimpleDialog(
-                          children: [
-                            Text('Введите токен'),
-                            TextField(
-                              onChanged: (value) => _token = value,
-                            ),
-                            RaisedButton(
-                              child: Text('Войти'),
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) => LoadingDialog(),
-                                );
-                                OrarioService.verify(token: _token)
-                                    .then((valid) {
-                                  if (valid) {
-                                    Navigator.pop(context);
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              ListScreen(isEditor: true),
-                                        ));
-                                  } else {
-                                    showDialog(
-                                      context: context,
-                                      builder: (context) {
-                                        return SimpleDialog(
-                                          children: [
-                                            Text('Неправильный токен')
-                                          ],
-                                        );
-                                      },
-                                    );
-                                  }
-                                });
-                              },
-                            )
-                          ],
-                        );
-                      },
-                    );
+                      break;
+                    case 1:
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          String _token;
+                          return SimpleDialog(
+                            children: [
+                              Text('Введите токен'),
+                              TextField(
+                                onChanged: (value) => _token = value,
+                              ),
+                              RaisedButton(
+                                child: Text('Войти'),
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => LoadingDialog(),
+                                  );
+                                  OrarioService.verify(token: _token)
+                                      .then((valid) {
+                                    if (valid) {
+                                      Navigator.pop(context);
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                ListScreen(isEditor: true),
+                                          ));
+                                    } else {
+                                      showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return SimpleDialog(
+                                            children: [
+                                              Text('Неправильный токен')
+                                            ],
+                                          );
+                                        },
+                                      );
+                                    }
+                                  });
+                                },
+                              )
+                            ],
+                          );
+                        },
+                      );
 
-                    break;
-                  default:
-                }
-              },
+                      break;
+                    default:
+                  }
+                },
+              ),
             );
           }),
     );
