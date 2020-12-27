45b10b76abd85d5309b4926cd2068af3c64fcb04
Mike <m.shevelinski@ya.ru>
2020-12-27 12:58:04 +0300

    Added icon to splash page

--- lib/presentation/splash_page.dart
+++ lib/presentation/splash_page.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('SplashPage'),
+    return Scaffold(
+      body: Center(
+        child: Image(image: AssetImage('assets/logo.png')),
+      ),
     );
   }
 }
