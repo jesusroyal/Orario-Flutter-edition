041768f4236ae07215dd1b693dc1117bdee452a4
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-05 16:54:01 +0300

    Added settings to welcome

--- lib/domain/bloc/welcome/welcome_bloc.dart
+++ lib/domain/bloc/welcome/welcome_bloc.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:orario/data/api/service/login/login_service.dart';
+import 'package:orario/data/api/service/settings/settings_service.dart';
 import 'package:orario/data/repository/login/login_data_repository.dart';
+import 'package:orario/data/repository/settings/settings_data_repository.dart';
 
 import 'welcome_event.dart';
 import 'welcome_state.dart';
@@ -9,42 +12,54 @@ class WelcomeBloc extends Bloc<WelcomeEvent, WelcomeState> {
   WelcomeBloc() : super((WelcomeInitial()));
 
   LoginDataRepository loginRepository = LoginDataRepository(LoginService());
+  SettingsDataRepository settings =
+      SettingsDataRepository(settingsService: SettingsService());
+
+  Map<String, String> universityMap = {};
+  Map<String, String> groupMap = {};
+
+  List<String> universityList = [];
+  List<String> groupList = [];
+
+  String uni = '';
+  String group = '';
+
+  List<String> mapToList(Map<String, String> map) {
+    List<String> list = [];
+    map.forEach((key, value) {
+      list.add(value);
+    });
+    return list;
+  }
 
   @override
   Stream<WelcomeState> mapEventToState(WelcomeEvent event) async* {
     //TODO: Refactor
     if (event is WelcomePressed) {
       yield WelcomeLoading();
-      var map = await loginRepository.getUniversities();
-      List<String> list = [];
-      map.forEach((key, value) {
-        list.add(value);
-      });
-      yield UniversityLoaded(universities: list);
+      universityMap = await loginRepository.getUniversities();
+      universityList = mapToList(universityMap);
+      yield UniversityLoaded(universities: universityList);
     }
     if (event is UniversityPressed) {
       yield GroupsLoading();
 
-      var mapUni = await loginRepository.getUniversities();
-      List<String> listUni = [];
-      mapUni.forEach((key, value) {
-        listUni.add(value);
-      });
-
-      var index = mapUni.keys.firstWhere(
-          (k) => mapUni[k] == listUni[event.index],
+      uni = universityMap.keys.firstWhere(
+          (k) => universityMap[k] == universityList[event.index],
           orElse: () => null);
 
-      var map = await loginRepository.getGroups(university: index);
-      List<String> list = [];
-      map.forEach((key, value) {
-        list.add(value);
-      });
+      groupMap = await loginRepository.getGroups(university: uni);
+      groupList = mapToList(groupMap);
 
-      yield GroupsLoaded(groups: list);
+      yield GroupsLoaded(groups: groupList);
     }
 
     if (event is GroupPressed) {
+      group = groupMap.keys.firstWhere(
+          (k) => groupMap[k] == groupList[event.index],
+          orElse: () => null);
+      await settings.setPath(path: '$uni/$group');
+      print('$uni/$group');
       yield WelcomeComplete();
     }
   }
