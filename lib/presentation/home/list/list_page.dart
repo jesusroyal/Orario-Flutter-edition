ac1e7cb89c56a5a3935cdc2a3054e10dab262871
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-06 22:29:09 +0300

    Added list widgets

---
+++ lib/presentation/home/list/list_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:orario/domain/bloc/home/home_list/home_list_bloc.dart';
+import 'package:orario/domain/bloc/home/home_list/home_list_state.dart';
+import 'package:orario/domain/bloc/home/home_list/home_list_event.dart';
+import 'package:group_list_view/group_list_view.dart';
+import 'package:orario/presentation/home/list/list_divider.dart';
+import 'package:orario/presentation/home/list/list_tile.dart';
+
+class ListPage extends StatefulWidget {
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  HomeListBloc homeListBloc;
+
+  @override
+  void initState() {
+    homeListBloc = HomeListBloc();
+    homeListBloc.add(HomeListOpened());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => homeListBloc,
+      child: Scaffold(
+        body: BlocBuilder<HomeListBloc, HomeListState>(
+          builder: (context, state) {
+            if (state is HomeListLoading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is HomeListLoaded) {
+              return GroupListView(
+                countOfItemInSection: (count) => 1,
+                sectionsCount: 1,
+                itemBuilder: (context, index) {
+                  return ListWidget(
+                    lessonPair: state.lessons[index.index],
+                  );
+                },
+                groupHeaderBuilder: (context, section) {
+                  return ListSection(section);
+                },
+              );
+            }
+            return Center(child: Text('Something went wrong'));
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: null, label: Text('Неделя II')),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/home/list/list_tile.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:orario/domain/model/lesson_pair.dart';
+
+class ListWidget extends StatelessWidget {
+  final LessonPair lessonPair;
+
+  const ListWidget({Key key, @required this.lessonPair}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(2.0),
+      child: Container(
+          height: 70.0,
+          color: Colors.white,
+          margin: EdgeInsets.all(7.0),
+          child: Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(lessonPair.time.start.format(context)),
+                  Text(
+                    lessonPair.time.end.format(context),
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              ),
+              VerticalDivider(
+                color: Colors.deepOrange, //TODO: Add colors
+                thickness: 1.0,
+              ),
+              SizedBox(
+                width: 5.0,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          lessonPair.lesson.name,
+                          style: null, //TODO: Add style
+                          textAlign: TextAlign.left,
+                        ),
+                        Text(
+                          lessonPair.lesson.don,
+                          textAlign: TextAlign.right,
+                          style: TextStyle(fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      lessonPair.lesson.location,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontWeight: FontWeight.w300),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
