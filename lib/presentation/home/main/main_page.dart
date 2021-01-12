81688e5303c13917cfbc7979ee35ced134399a35
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-12 10:57:20 +0300

    Add appbar to main page

--- lib/presentation/home/main/main_page.dart
+++ lib/presentation/home/main/main_page.dart
@@ -5,14 +5,18 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [IconButton(icon: Icon(Icons.settings), onPressed: null)],
+        elevation: 0.0,
+      ),
       body: LayoutBuilder(
         builder: (context, constraints) {
           return Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 child: LessonCardContainer(),
-                height: constraints.maxHeight * (0.45),
+                height: constraints.maxHeight * (0.4),
               ),
               Container(
                   height: constraints.maxHeight * (0.30),
