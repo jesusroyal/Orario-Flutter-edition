a6d80832521e544db47af2cdbc3d75ca85289fdc
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-12 11:01:30 +0300

    Add home tomorrow list bloc

---
+++ lib/domain/bloc/home/home_tomorrow_list/home_tomorrow_list_event.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+abstract class HomeTomorrowListEvent extends Equatable {
+  const HomeTomorrowListEvent();
+
+  @override
+  List<Object> get props => [];
+}
