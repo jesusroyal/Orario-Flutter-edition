5b03647ba35bbfd94bbed692fb4538646f2d0391
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-16 13:58:47 +0300

    Add navigation to settings

--- lib/presentation/home/main/main_page.dart
+++ lib/presentation/home/main/main_page.dart
@@ -2,13 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:orario/domain/bloc/home/home_tomorrow_list/home_tomorrow_list_bloc.dart';
 import 'package:orario/presentation/home/main/current_widget/current_swiper.dart';
 import 'package:orario/presentation/home/main/tomorrow_widget/tomorrow_widget.dart';
+import 'package:orario/presentation/home/settings.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [IconButton(icon: Icon(Icons.settings), onPressed: null)],
+        actions: [
+          IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsPage(),
+                  ),
+                );
+              })
+        ],
         elevation: 0.0,
       ),
       body: LayoutBuilder(
