0606704a386fd87dbc84e3d51bea0747a0f119e4
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-18 13:03:23 +0300

    Add lesson page

---
+++ lib/presentation/home/admin/lesson_page/lesson_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LessonPage extends StatefulWidget {
+  @override
+  _LessonPageState createState() => _LessonPageState();
+}
+
+class _LessonPageState extends State<LessonPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('temp'),
+    );
+  }
+}
