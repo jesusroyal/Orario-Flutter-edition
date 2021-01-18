daa8e08eea2d5421527343d5e36bc04cc03782b0
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-18 13:02:57 +0300

    Add Admin page

--- lib/presentation/home/admin/admin_page.dart
+++ lib/presentation/home/admin/admin_page.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:orario/presentation/home/admin/lesson_page/lesson_page.dart';
+import 'package:orario/presentation/home/admin/lesson_time_page/lesson_time_page.dart';
 
 class AdminPage extends StatelessWidget {
+  Widget lessonTimeEditTile(BuildContext context) {
+    return Card(
+        child: ListTile(
+            title: Text('Расписания звонков'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LessonTimePage(),
+                ),
+              );
+            }));
+  }
+
+  Widget lessonEditTile(BuildContext context) {
+    return Card(
+        child: ListTile(
+            title: Text('Расписания занятий'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LessonPage(),
+                ),
+              );
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> tiles = [lessonTimeEditTile(context), lessonEditTile(context)];
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Admin'),
-      ),
-    );
+        appBar: AppBar(),
+        body: ListView.builder(
+            itemCount: tiles.length,
+            itemBuilder: (context, index) {
+              return tiles[index];
+            }));
   }
 }
