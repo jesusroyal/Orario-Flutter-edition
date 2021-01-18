6d9c2ed3bd5e4506b1365e19862659d1a33304b6
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-18 11:54:00 +0300

    Change tile store

--- lib/presentation/home/settings/settings.dart
+++ lib/presentation/home/settings/settings.dart
@@ -4,6 +4,7 @@ import 'package:orario/data/repository/settings/settings_data_repository.dart';
 import 'package:orario/domain/bloc/settings/settings_bloc.dart';
 import 'package:orario/domain/bloc/settings/settings_event.dart';
 import 'package:orario/domain/bloc/settings/settings_state.dart';
+import 'package:orario/presentation/home/admin/admin_page.dart';
 import 'package:orario/presentation/home/settings/settings_tiles/about_widget.dart';
 import 'package:orario/presentation/home/settings/settings_tiles/admin_widget.dart';
 import 'package:orario/presentation/home/settings/settings_tiles/reset_widget.dart';
@@ -15,13 +16,16 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  List<Widget> tileList = [AboutSettingsTile(), AdminTile()];
+  List<Widget> tileList = [AboutSettingsTile()];
 
   SettingsBloc bloc;
 
   @override
   void initState() {
     bloc = SettingsBloc();
+    tileList.add(AdminTile(onSubmit: (token) {
+      bloc.add(AdminPageOpen(token: token));
+    }));
     tileList.add(ResetSettingsTile(
       onTap: () {
         bloc.add(SettingsReset());
@@ -49,6 +53,28 @@ class _SettingsPageState extends State<SettingsPage> {
           if (state is ResetSettingsState) {
             Navigator.pushReplacement(context,
                 MaterialPageRoute(builder: (context) => WelcomePage()));
+          }
+          if (state is SettingsLoading) {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Zagruzka'),
+                  );
+                });
+          }
+          if (state is TokenError) {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Owibka'),
+                  );
+                });
+          }
+          if (state is OpenAdminPanel) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => AdminPage()));
           }
         },
       ),
