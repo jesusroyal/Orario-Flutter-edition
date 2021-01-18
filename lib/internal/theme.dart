9522d252cbe27dcc1735fb013085b3dc58ba3578
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-18 14:54:51 +0300

    Add more text styles

--- lib/internal/theme.dart
+++ lib/internal/theme.dart
@@ -20,6 +20,9 @@ class OrarioTextStyle {
   final TextStyle h2Bold = TextStyle(fontSize: 34, fontWeight: FontWeight.w900);
   final TextStyle h2Light =
       TextStyle(fontSize: 34, fontWeight: FontWeight.w200);
+  final TextStyle h3Bold = TextStyle(fontSize: 24, fontWeight: FontWeight.w900);
   final TextStyle h3Light =
       TextStyle(fontSize: 24, fontWeight: FontWeight.w200);
+  final TextStyle h4 = TextStyle(fontSize: 18);
+  final TextStyle h4Bold = TextStyle(fontSize: 18, fontWeight: FontWeight.w900);
 }
