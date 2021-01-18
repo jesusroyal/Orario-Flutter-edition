fa5e2eb2a202be2759692225b890633e4799261c
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-18 14:51:31 +0300

    Add text styles

--- lib/internal/theme.dart
+++ lib/internal/theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class OrarioUI {
   static final OrarioUI ref = OrarioUI();
   static final colors = OrarioColor();
+  static final text = OrarioTextStyle();
   ThemeData lightTheme = ThemeData(
       backgroundColor: colors.background,
       scaffoldBackgroundColor: colors.background,
@@ -12,4 +13,13 @@ class OrarioUI {
 class OrarioColor {
   final Color background = Color(0xFFECF3F5);
   final Color green = Color(0xFF01A7C2);
+}
+
+class OrarioTextStyle {
+  final TextStyle h1Bold = TextStyle(fontSize: 64, fontWeight: FontWeight.w900);
+  final TextStyle h2Bold = TextStyle(fontSize: 34, fontWeight: FontWeight.w900);
+  final TextStyle h2Light =
+      TextStyle(fontSize: 34, fontWeight: FontWeight.w200);
+  final TextStyle h3Light =
+      TextStyle(fontSize: 24, fontWeight: FontWeight.w200);
 }
