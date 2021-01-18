a45515e5719f64a6f0e856d3bc4c47bd638a6e28
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-18 13:03:31 +0300

    Add lesson time page

---
+++ lib/presentation/home/admin/lesson_time_page/lesson_time_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LessonTimePage extends StatefulWidget {
+  @override
+  _LessonTimePageState createState() => _LessonTimePageState();
+}
+
+class _LessonTimePageState extends State<LessonTimePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
