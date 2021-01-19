8511c992dd80450469a872e8786455f1b832ac3c
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-19 13:44:34 +0300

    Add init

--- lib/data/api/model/api_lesson_time.dart
+++ lib/data/api/model/api_lesson_time.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/material.dart';
+
 class ApiLessonTime {
   final int start;
   final int end;
   final int breakDuration;
+
+  ApiLessonTime(
+      {@required this.start, @required this.end, @required this.breakDuration});
 
   ApiLessonTime.fromApi(dynamic map)
       : start = int.parse(map['start']),
