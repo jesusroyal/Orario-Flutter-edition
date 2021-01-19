db9addc0894292f9a4f015ed316aceb8d3cac08b
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-19 13:44:44 +0300

    Add toApi mapping

--- lib/data/mapper/lesson_time_mapper.dart
+++ lib/data/mapper/lesson_time_mapper.dart
@@ -3,13 +3,22 @@ import 'package:orario/domain/model/model_export.dart';
 import 'package:orario/data/api/model/api_model_export.dart';
 
 class LessonTimeMapper {
-  static LessonTime fromApi(ApiLessonTime apiLessonTime){
+  static LessonTime fromApi(ApiLessonTime apiLessonTime) {
     TimeOfDay start = TimeOfDay(
-        hour: (apiLessonTime.start ~/ 60), minute: (apiLessonTime.start - (apiLessonTime.start ~/ 60) * 60));
-    TimeOfDay end =
-        TimeOfDay(hour: (apiLessonTime.end ~/ 60), minute: (apiLessonTime.end - (apiLessonTime.end ~/ 60) * 60));
+        hour: (apiLessonTime.start ~/ 60),
+        minute: (apiLessonTime.start - (apiLessonTime.start ~/ 60) * 60));
+    TimeOfDay end = TimeOfDay(
+        hour: (apiLessonTime.end ~/ 60),
+        minute: (apiLessonTime.end - (apiLessonTime.end ~/ 60) * 60));
 
-        return LessonTime(start: start, end: end, breakDuration: apiLessonTime.breakDuration);
+    return LessonTime(
+        start: start, end: end, breakDuration: apiLessonTime.breakDuration);
+  }
+
+  static ApiLessonTime toApi(LessonTime lessonTime) {
+    return ApiLessonTime(
+        start: ((lessonTime.start.hour * 60) + lessonTime.start.minute),
+        end: ((lessonTime.end.hour * 60) + lessonTime.end.minute),
+        breakDuration: 0);
   }
 }
-
