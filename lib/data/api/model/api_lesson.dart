6ec96763b7e7585846e550170970b641e290dff2
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-22 14:27:34 +0300

    Add to api method

--- lib/data/api/model/api_lesson.dart
+++ lib/data/api/model/api_lesson.dart
@@ -5,8 +5,12 @@ class ApiLesson {
   final int type;
 
   ApiLesson.fromApi(Map<String, dynamic> map)
-    : name = map['name'],
-      location = map['location'],
-      don = map['don'],
-      type = map['type'];
+      : name = map['name'],
+        location = map['location'],
+        don = map['don'],
+        type = map['type'];
+
+  Map<String, dynamic> toApi() {
+    return {'name': name, 'location': location, 'don': don, 'type': type};
+  }
 }
