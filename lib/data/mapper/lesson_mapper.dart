3eebd91a6a804b48f47387571efb621870000dd5
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-22 14:49:48 +0300

    Add Lesson to ApiLesson

--- lib/data/mapper/lesson_mapper.dart
+++ lib/data/mapper/lesson_mapper.dart
@@ -1,10 +1,10 @@
-import 'package:orario/domain/model/model_export.dart';
 import 'package:orario/data/api/model/api_model_export.dart';
+import 'package:orario/domain/model/model_export.dart';
 
 class LessonMapper {
   static Lesson fromApi(ApiLesson apiLesson) {
-      LessonType type;
-      switch (apiLesson.type) {
+    LessonType type;
+    switch (apiLesson.type) {
       case 0:
         {
           type = LessonType.lecture;
@@ -26,9 +26,41 @@ class LessonMapper {
         }
         break;
     }
-    return Lesson(name: apiLesson.name,
-                  location: apiLesson.location,
-                  don: apiLesson.don, 
-                  type: type);
+    return Lesson(
+        name: apiLesson.name,
+        location: apiLesson.location,
+        don: apiLesson.don,
+        type: type);
+  }
+
+  static ApiLesson toApi(Lesson lesson) {
+    int lessonType = 0;
+    switch (lesson.type) {
+      case LessonType.lecture:
+        {
+          lessonType = 0;
+        }
+        break;
+      case LessonType.lab:
+        {
+          lessonType = 1;
+        }
+        break;
+      case LessonType.seminar:
+        {
+          lessonType = 2;
+        }
+        break;
+      default:
+        {
+          lessonType = 0;
+        }
+        break;
+    }
+    return ApiLesson(
+        name: lesson.name,
+        location: lesson.location,
+        don: lesson.don,
+        type: lessonType);
   }
 }
