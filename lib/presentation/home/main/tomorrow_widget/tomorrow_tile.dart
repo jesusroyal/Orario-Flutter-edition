38abc68c2a2f0ce98802ab09d33c77223ee60c6f
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-02-06 18:00:34 +0300

    Fix style

--- lib/presentation/home/main/tomorrow_widget/tomorrow_tile.dart
+++ lib/presentation/home/main/tomorrow_widget/tomorrow_tile.dart
@@ -10,20 +10,19 @@ class TomorrowTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         child: Container(
-            margin: EdgeInsets.all(10.0),
+            margin: const EdgeInsets.all(10.0),
             width: 150.0,
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(lessonPair.time.start.format(context)),
                   Text(
                     lessonPair.lesson.name, //TODO: Make short name
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    style: const TextStyle(
+                        fontSize: 20.0, fontWeight: FontWeight.bold),
                   ),
                   Text(lessonPair.lesson.location),
-                  Divider(
+                  const Divider(
                     color: Colors.amberAccent, //TODO: Add colors
                     thickness: 1.0,
                   )
