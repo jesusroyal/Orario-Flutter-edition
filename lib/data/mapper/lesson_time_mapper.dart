006433e22310285125270b85bdc9d72f7522a36f
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-02-06 18:06:46 +0300

    Remove unnecessary code

--- lib/data/mapper/lesson_time_mapper.dart
+++ lib/data/mapper/lesson_time_mapper.dart
@@ -5,11 +5,11 @@ import 'package:orario/data/api/model/api_model_export.dart';
 class LessonTimeMapper {
   static LessonTime fromApi(ApiLessonTime apiLessonTime) {
     final TimeOfDay start = TimeOfDay(
-        hour: (apiLessonTime.start ~/ 60),
-        minute: (apiLessonTime.start - (apiLessonTime.start ~/ 60) * 60));
-    TimeOfDay end = TimeOfDay(
-        hour: (apiLessonTime.end ~/ 60),
-        minute: (apiLessonTime.end - (apiLessonTime.end ~/ 60) * 60));
+        hour: apiLessonTime.start ~/ 60,
+        minute: apiLessonTime.start - (apiLessonTime.start ~/ 60) * 60);
+    final TimeOfDay end = TimeOfDay(
+        hour: apiLessonTime.end ~/ 60,
+        minute: apiLessonTime.end - (apiLessonTime.end ~/ 60) * 60);
 
     return LessonTime(
         start: start, end: end, breakDuration: apiLessonTime.breakDuration);
@@ -17,8 +17,8 @@ class LessonTimeMapper {
 
   static ApiLessonTime toApi(LessonTime lessonTime) {
     return ApiLessonTime(
-        start: ((lessonTime.start.hour * 60) + lessonTime.start.minute),
-        end: ((lessonTime.end.hour * 60) + lessonTime.end.minute),
+        start: (lessonTime.start.hour * 60) + lessonTime.start.minute,
+        end: (lessonTime.end.hour * 60) + lessonTime.end.minute,
         breakDuration: 0);
   }
 }
