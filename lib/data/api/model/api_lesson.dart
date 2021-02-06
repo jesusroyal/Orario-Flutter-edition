9357fe972be7096f87aaa1fd9d04d99dd1baa029
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-02-06 13:40:01 +0300

    Fix types

--- lib/data/api/model/api_lesson.dart
+++ lib/data/api/model/api_lesson.dart
@@ -1,18 +1,18 @@
 class ApiLesson {
+  ApiLesson({this.name, this.location, this.don, this.type});
+
   final String name;
   final String location;
   final String don;
   final int type;
 
   ApiLesson.fromApi(Map<String, dynamic> map)
-      : name = map['name'],
-        location = map['location'],
-        don = map['don'],
-        type = map['type'];
+      : name = map['name'] as String,
+        location = map['location'] as String,
+        don = map['don'] as String,
+        type = map['type'] as int;
 
   Map<String, dynamic> toApi() {
     return {'name': name, 'location': location, 'don': don, 'type': type};
   }
-
-  ApiLesson({this.name, this.location, this.don, this.type});
 }
