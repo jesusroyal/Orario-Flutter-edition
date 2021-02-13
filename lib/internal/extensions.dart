97e1e1dd1b7834274161243b309a88003a5e40b6
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-02-13 14:06:14 +0300

    Remove unused this

--- lib/internal/extensions.dart
+++ lib/internal/extensions.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 extension WeekNumber on DateTime {
   int get weekNumber {
-    final startOfYear = DateTime(this.year, 1, 1, 0, 0);
+    final startOfYear = DateTime(year, 1, 1, 0, 0);
     final firstMonday = startOfYear.weekday;
     final daysInFirstWeek = 8 - firstMonday;
-    final diff = this.difference(startOfYear);
+    final diff = difference(startOfYear);
     var weeks = ((diff.inDays - daysInFirstWeek) / 7).ceil();
     if (daysInFirstWeek > 3) {
       weeks += 1;
