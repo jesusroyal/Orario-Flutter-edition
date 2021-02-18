d194d7e56586c60d1fbf8e43ef846d7161b31ed8
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-02-18 22:18:41 +0300

    Add string shortner

--- lib/internal/extensions.dart
+++ lib/internal/extensions.dart
@@ -19,3 +19,64 @@ extension Minutes on TimeOfDay {
     return hour * 60 + minute;
   }
 }
+
+extension Short on String {
+  String get short {
+    final List<String> consonants = [
+      'б',
+      'в',
+      'г',
+      'д',
+      'ж',
+      'з',
+      'й',
+      'к',
+      'л',
+      'м',
+      'н',
+      'п',
+      'р',
+      'с',
+      'т',
+      'ф',
+      'х',
+      'ц',
+      'щ'
+    ];
+
+    final String input = this;
+    if (input.length <= 10) {
+      return input;
+    }
+
+    if (input.length < 24 || input.split(' ').length < 3) {
+      String output = '';
+      final List<String> words = input.split(' ');
+
+      words.forEach((word) {
+        if (word.length < 4) {
+          output += word + ' ';
+        } else {
+          output += word.substring(0, 3);
+          if (word.length > 4) {
+            int i = 1;
+            while (consonants.contains(word[i + 3])) {
+              output += word[i + 3];
+              i++;
+            }
+          }
+
+          output += '. ';
+        }
+      });
+      return output;
+    }
+
+    List<String> words = input.split(" ");
+    String output = '';
+    words.forEach((word) {
+      output += word[0];
+    });
+    return output;
+  }
+}
